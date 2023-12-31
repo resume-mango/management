6da69df7dcc6c41d235f6a5336d1547029201595
kundanbhosale <kundanmbhosale@gmail.com>
2023-12-31 23:37:05 +0530

    fix

--- src/validations/plans.ts
+++ src/validations/plans.ts
@@ -1,25 +1,25 @@
-import * as Yup from 'yup'
+import * as Yup from "yup"
 
 const name = Yup.string()
-  .max(127, 'max 127 characters allowed!')
-  .required('Required')
+  .max(127, "max 127 characters allowed!")
+  .required("Required")
 const description = Yup.string()
-  .max(127, 'max 127 characters allowed!')
-  .required('Required')
+  .max(127, "max 127 characters allowed!")
+  .required("Required")
 const label = Yup.string()
-  .max(127, 'max 127 characters allowed!')
-  .required('Required')
-const type = Yup.string().oneOf(['starter', 'pro', 'ceo']).required()
+  .max(127, "max 127 characters allowed!")
+  .required("Required")
+const type = Yup.string().oneOf(["starter", "ceo"]).required()
 const price = Yup.number()
-  .typeError('Invalid Number')
-  .min(0, 'Min 0 value allowed')
-  .required('Required')
+  .typeError("Invalid Number")
+  .min(0, "Min 0 value allowed")
+  .required("Required")
 const highlights = Yup.array().of(
   Yup.object().shape({
     key: Yup.string()
-      .max(256, 'Max 256 characters allowed!')
-      .required('Required'),
-    bool: Yup.boolean().required('Required'),
+      .max(256, "Max 256 characters allowed!")
+      .required("Required"),
+    bool: Yup.boolean().required("Required"),
   })
 )
 
