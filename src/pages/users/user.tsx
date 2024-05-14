bf6779de87939a5838011de6116ef89834c28e18
Kundan Bhosale <kundanmbhosale@gmail.com>
2024-05-15 01:41:12 +0530

    Updates

--- src/pages/users/user.tsx
+++ src/pages/users/user.tsx
@@ -1,31 +1,30 @@
-import React, { Fragment } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useParams } from 'react-router-dom'
-import styled from 'styled-components'
-import BackArrow from '../../components/svgs/backArrow'
-import SubNavBar from '../../components/ui/subNavbar'
-import { getSingleUser } from '../../queries/userQueries'
-import { Button } from '../../styled/button'
-import { LoadingDots, LoadingWrapper, Spinner } from '../../styled/loader'
-import Details from './details'
-import Payments from './payments'
-import UserSubscriptions from './subscriptions'
+import React, { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import BackArrow from "../../components/svgs/backArrow";
+import SubNavBar from "../../components/ui/subNavbar";
+import { getSingleUser } from "../../queries/userQueries";
+import { Button } from "../../styled/button";
+import { LoadingDots, LoadingWrapper, Spinner } from "../../styled/loader";
+import Details from "./details";
+import Payments from "./payments";
+import UserSubscriptions from "./subscriptions";
 
 const SingleUser = () => {
-  const { user_id, type } = useParams()
-  const navigate = useNavigate()
+  const { user_id, type } = useParams();
+  const navigate = useNavigate();
   const { data, isLoading, isError } = getSingleUser(
-    user_id ? user_id : '',
+    user_id ? user_id : "",
     !!user_id
-  )
-  const handleType = (type: 'details' | 'subscriptions' | 'payments') => {
-    navigate(`/users/${user_id}/${type}`)
-  }
-
+  );
+  const handleType = (type: "details" | "subscriptions" | "payments") => {
+    navigate(`/users/${user_id}/${type}`);
+  };
   return (
     <Fragment>
       {!user_id || isError ? (
-        <div className="align-center" style={{ height: '30vh' }}>
+        <div className="align-center" style={{ height: "30vh" }}>
           <h3>Failed to load user data!</h3>
         </div>
       ) : isLoading ? (
@@ -35,12 +34,12 @@ const SingleUser = () => {
         </LoadingWrapper>
       ) : data && data.user ? (
         <Wrapper>
-          <div style={{ padding: '0 1.5rem' }}>
+          <div style={{ padding: "0 1.5rem" }}>
             <Button
               btnType="ghost"
               size="lg"
-              style={{ width: 'fit-content', padding: '0 1rem' }}
-              onClick={() => navigate('/users')}
+              style={{ width: "fit-content", padding: "0 1rem" }}
+              onClick={() => navigate("/users")}
             >
               <BackArrow size="1.4rem" />
             </Button>
@@ -53,56 +52,56 @@ const SingleUser = () => {
               <h2>{data.user.name}</h2>
               <p>
                 User ID:&nbsp;&nbsp;
-                {(data.user.app_metadata && data.user.app_metadata.ref) || '-'}
+                {data.user.user_id || "-"}
               </p>
             </div>
           </Header>
           <SubNavBar>
             <SubNavBar.Link>
               <a
-                onClick={() => handleType('details')}
-                className={type === 'details' ? 'active' : ''}
+                onClick={() => handleType("details")}
+                className={type === "details" ? "active" : ""}
               >
                 Details
               </a>
             </SubNavBar.Link>
             <SubNavBar.Link>
               <a
-                onClick={() => handleType('subscriptions')}
-                className={type === 'subscriptions' ? 'active' : ''}
+                onClick={() => handleType("subscriptions")}
+                className={type === "subscriptions" ? "active" : ""}
               >
                 Subscriptions
               </a>
             </SubNavBar.Link>
             <SubNavBar.Link>
               <a
-                onClick={() => handleType('payments')}
-                className={type === 'payments' ? 'active' : ''}
+                onClick={() => handleType("payments")}
+                className={type === "payments" ? "active" : ""}
               >
                 Payments
               </a>
             </SubNavBar.Link>
           </SubNavBar>
           <Container>
-            {type === 'details' ? (
+            {type === "details" ? (
               <Details
                 user={data.user}
                 localUser={data.localUser}
                 roles={data.roles}
               />
-            ) : type === 'subscriptions' ? (
-              <UserSubscriptions id={data.localUser._id} />
-            ) : type === 'payments' ? (
-              <Payments id={data.localUser._id} />
+            ) : type === "subscriptions" ? (
+              <UserSubscriptions id={data.user.user_id} />
+            ) : type === "payments" ? (
+              <Payments id={data.user.user_id} />
             ) : null}
           </Container>
         </Wrapper>
       ) : null}
     </Fragment>
-  )
-}
+  );
+};
 
-export default SingleUser
+export default SingleUser;
 
 const Wrapper = styled.div`
   padding: 2rem 0;
@@ -110,7 +109,7 @@ const Wrapper = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Header = styled.div`
   padding: 1.5rem;
@@ -137,9 +136,9 @@ const Header = styled.div`
       height: 100%;
     }
   }
-`
+`;
 const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-`
+`;
